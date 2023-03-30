39e3830a49471670b40302baccac06965da2aa04
Giacomo <s301311@studenti.polito.it>
2023-03-30 21:34:49 +0200

    feat: database connection and queries

---
+++ server/src/main/resources/records.sql
@@ -0,0 +1,7 @@
+insert into customers(email, name, surname) values ('mario.rossi@polito.it', 'Mario', 'Rossi');
+insert into customers(email, name, surname) values ('john.doe@polito.it', 'John', 'Doe');
+insert into customers(email, name, surname) values ('jane.doe@polito.it', 'Jane', 'Doe');
+
+insert into products(product_id, name, brand) values ('1234512345123', 'PC', 'HP');
+insert into products(product_id, name, brand) values ('5432154321321', 'iPhone', 'Apple');
+insert into products(product_id, name, brand) values ('1234567890123', 'smartphone', 'Samsung');
---
+++ server/src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+create table if not exists customers(
+    email varchar(255) primary key,
+    name varchar (255),
+    surname varchar(255)
+);
+
+create table if not exists products(
+    product_id varchar(15) primary key,
+    name varchar (255),
+    brand varchar(255)
+);
