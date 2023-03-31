b6fc8ddf5cdafc7b2c45b6dd733fd75d6eef3e9b
flaviociravegna <s303398@studenti.polito.it>
2023-03-31 17:07:52 +0200

    Feat: profile exceptions implemented

--- server/src/main/kotlin/it/polito/wa2/server/products/ProductServiceImpl.kt
+++ server/src/main/kotlin/it/polito/wa2/server/products/ProductServiceImpl.kt
@@ -4,9 +4,7 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class ProductServiceImpl(
-        private val productRepository: ProductRepository
-): ProductService {
+class ProductServiceImpl(private val productRepository: ProductRepository): ProductService {
     override fun getAllProducts(): List<ProductDTO> {
         return productRepository.findAll().map {it.toDTO()}
     }
--- server/src/main/kotlin/it/polito/wa2/server/profiles/ProfileController.kt
+++ server/src/main/kotlin/it/polito/wa2/server/profiles/ProfileController.kt
@@ -1,29 +1,53 @@
 package it.polito.wa2.server.profiles
 
+import it.polito.wa2.server.DuplicateProfileException
+import it.polito.wa2.server.ProfileNotFoundException
+import it.polito.wa2.server.UnprocessableProfileException
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ProfileController(
-        private val profileService: ProfileService
-) {
-
+class ProfileController(private val profileService: ProfileService) {
     @GetMapping("/API/profiles/{email}")
     fun getProfile(@PathVariable email: String): ProfileDTO? {
+        if (!email.matches(Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$")))
+            throw UnprocessableProfileException("Wrong email format")
+
         return profileService.getProfile(email)
+            ?: throw ProfileNotFoundException("Profile with email '${email} not found")
     }
 
     @PostMapping("/API/profiles")
-    fun addProfile(@RequestBody profile: ProfileDTO?) {
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addProfile(@RequestBody @Valid profile: ProfileDTO?, br: BindingResult) {
+        if (br.hasFieldErrors("email"))
+            throw UnprocessableProfileException("Wrong email format")
+        if (profile != null && profileService.getProfile(profile.email) == null)
+            throw DuplicateProfileException("Profile with email '${profile.email}' already exists")
         profileService.addProfile(profile!!)
     }
 
     @PutMapping("/API/profiles/{email}")
-    fun updateProfile(@PathVariable email: String, @RequestBody profile: ProfileDTO?) {
+    fun updateProfile(@PathVariable email: String, @RequestBody profile: ProfileDTO?, br: BindingResult) {
+        if (!email.matches(Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$")))
+            throw UnprocessableProfileException("Wrong email format")
+        if (br.hasErrors())
+            throw UnprocessableProfileException(br
+                .allErrors
+                .map { e -> e.objectName + ": " + e.defaultMessage + "\n" }
+                .reduce { s1, s2 -> s1 + s2 }
+            )
+        if (profileService.getProfile(email) == null)
+            throw ProfileNotFoundException("Profile with email '${email} not found")
+
         profileService.updateProfile(email, profile!!)
     }
 }
