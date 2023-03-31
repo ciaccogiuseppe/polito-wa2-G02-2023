fa174228eb59b2e72fd71773917311e56dc14581
flaviociravegna <s303398@studenti.polito.it>
2023-03-31 15:46:23 +0200

    Feat: product exceptions implemented

--- server/src/main/kotlin/it/polito/wa2/server/products/ProductController.kt
+++ server/src/main/kotlin/it/polito/wa2/server/products/ProductController.kt
@@ -1,21 +1,21 @@
 package it.polito.wa2.server.products
 
+import it.polito.wa2.server.ProductNotFoundException
+import it.polito.wa2.server.UnprocessableProductException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ProductController(
-        private val productService: ProductService
-) {
-
+class ProductController(private val productService: ProductService) {
     @GetMapping("/API/products/")
-    fun getAllProducts(): List<ProductDTO> {
-        return productService.getAllProducts()
-    }
+    fun getAllProducts(): List<ProductDTO> = productService.getAllProducts()
 
     @GetMapping("/API/products/{productId}")
     fun getProduct(@PathVariable productId: String): ProductDTO? {
+        if (!productId.matches("\\d{13}".toRegex()))
+            throw UnprocessableProductException("Wrong format for productId")
         return productService.getProduct(productId)
+            ?: throw ProductNotFoundException("Product with id '${productId}' not found")
     }
 }
--- server/src/main/kotlin/it/polito/wa2/server/products/ProductDTO.kt
+++ server/src/main/kotlin/it/polito/wa2/server/products/ProductDTO.kt
@@ -1,9 +1,15 @@
 package it.polito.wa2.server.products
 
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+
 data class ProductDTO(
-        val productId: String,
-        val name: String,
-        val brand: String
+    @Size(min = 13, max = 13)
+    val productId: String,
+    @NotNull
+    val name: String,
+    @NotNull
+    val brand: String
 )
 
 fun Product.toDTO(): ProductDTO {
