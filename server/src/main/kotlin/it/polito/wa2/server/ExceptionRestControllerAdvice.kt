45d6c8aa29645cdf56177dac6e15a43b36a17d46
Giacomo <s301311@studenti.polito.it>
2023-04-01 18:01:47 +0200

    feat: added new exception for profile bad request

--- server/src/main/kotlin/it/polito/wa2/server/ExceptionRestControllerAdvice.kt
+++ server/src/main/kotlin/it/polito/wa2/server/ExceptionRestControllerAdvice.kt
@@ -6,40 +6,44 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
-class ExceptionRestControllerAdvice {
+class ExceptionRestControllerAdvice: ResponseEntityExceptionHandler() {
     /************** Product exception handlers **************/
-    @ResponseBody
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    // @ResponseBody
+    // @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException::class)
     fun handleProductNotFound(e: ProductNotFoundException) = ProblemDetail.forStatusAndDetail( HttpStatus.NOT_FOUND, e.message!! )
 
-    @ResponseBody
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    //@ResponseBody
+    //@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(UnprocessableProductException::class)
     fun handleUnprocessableProduct(e: UnprocessableProductException) = ProblemDetail.forStatusAndDetail( HttpStatus.UNPROCESSABLE_ENTITY, e.message!! )
 
-    @ResponseBody
-    @ResponseStatus(HttpStatus.CONFLICT)
+    //@ResponseBody
+    //@ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DuplicateProductException::class)
     fun handleDuplicateProduct(e: DuplicateProductException) = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.message!! )
 
     /************** Profile exception handlers **************/
-    @ResponseBody
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    //@ResponseBody
+    //@ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProfileNotFoundException::class)
     fun handleProfileNotFound(e: ProfileNotFoundException) = ProblemDetail.forStatusAndDetail( HttpStatus.NOT_FOUND, e.message!! )
 
-    @ResponseBody
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    //@ResponseBody
+    //@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(UnprocessableProfileException::class)
     fun handleUnprocessableProfile(e: UnprocessableProfileException) = ProblemDetail.forStatusAndDetail( HttpStatus.UNPROCESSABLE_ENTITY, e.message!! )
 
-    @ResponseBody
-    @ResponseStatus(HttpStatus.CONFLICT)
+    //@ResponseBody
+    //@ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DuplicateProfileException::class)
     fun handleDuplicateProfile(e: DuplicateProfileException) = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.message!! )
+
+    @ExceptionHandler(BadRequestProfileException::class)
+    fun handleProfileBadRequest(e: BadRequestProfileException) = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.message!! )
 }
 
 class ProductNotFoundException(message: String): RuntimeException(message) {}
@@ -48,3 +52,4 @@ class DuplicateProductException(message: String): RuntimeException(message) {}
 class ProfileNotFoundException(message: String): RuntimeException(message) {}
 class UnprocessableProfileException(message: String): RuntimeException(message) {}
 class DuplicateProfileException(message: String): RuntimeException(message) {}
+class BadRequestProfileException(message: String): RuntimeException(message) {}
