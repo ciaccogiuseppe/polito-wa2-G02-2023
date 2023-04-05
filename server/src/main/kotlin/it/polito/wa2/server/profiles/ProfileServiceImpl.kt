3fde3b92ecf99e1646931d8c9fb531218eb0d6c4
Giacomo <s301311@studenti.polito.it>
2023-04-05 11:43:40 +0200

    fix: updated functions in ProfileServiceImpl

--- server/src/main/kotlin/it/polito/wa2/server/profiles/ProfileServiceImpl.kt
+++ server/src/main/kotlin/it/polito/wa2/server/profiles/ProfileServiceImpl.kt
@@ -1,5 +1,7 @@
 package it.polito.wa2.server.profiles
 
+import it.polito.wa2.server.DuplicateProfileException
+import it.polito.wa2.server.ProfileNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,16 +15,22 @@ class ProfileServiceImpl(
     }
 
     override fun addProfile(profile: ProfileDTO) {
+        if (profileRepository.findByEmail(profile.email) != null)
+            throw DuplicateProfileException("Profile with email '${profile.email}' already exists")
         profileRepository.save(profile.toProfile())
    }
 
     override fun updateProfile(email: String, newProfile: ProfileDTO) {
+        if(profileRepository.findByEmail(newProfile.email) != null)
+            throw DuplicateProfileException("Profile with email '${newProfile.email}' already exists")
         val profile = profileRepository.findByEmail(email)
         if(profile != null) {
             profile.email = newProfile.email
             profile.name = newProfile.name
             profile.surname = newProfile.surname
             profileRepository.save(profile)
+        } else {
+            throw ProfileNotFoundException("Profile with email '${email}' not found")
         }
     }
 }
