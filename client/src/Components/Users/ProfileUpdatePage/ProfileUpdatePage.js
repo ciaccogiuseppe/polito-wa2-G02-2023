4f19cf524086daa327c32bd7d6e80edeb9525b71
GIUSEPPE <s295982@studenti.polito.it>
2023-04-20 10:18:45 +0200

    feat: 2 step profile update

--- client/src/Components/Users/ProfileUpdatePage/ProfileUpdatePage.js
+++ client/src/Components/Users/ProfileUpdatePage/ProfileUpdatePage.js
@@ -1,19 +1,48 @@
 import {Button, Form, Spinner} from "react-bootstrap";
 import AppNavbar from "../../AppNavbar/AppNavbar";
 import {useEffect, useState} from "react";
-import {addNewProfile, editProfile} from "../../../API/Profiles";
+import {addNewProfile, editProfile, getProfileDetails} from "../../../API/Profiles";
 
 
 
 
 function ProfileUpdatePage(props){
     const [oldEmail, setOldEmail] = useState("");
+    const [fetched, setFetched] = useState(false);
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [surname, setSurname] = useState("");
     const [errMessage, setErrMessage] = useState("");
     const [response, setResponse] = useState("");
     const [loading, setLoading] = useState(false);
+
+    function getProfile(){
+        setLoading(true);
+        setResponse("");
+        getProfileDetails(oldEmail).then(
+            res => {
+                setErrMessage("");
+                setName(res.name);
+                setSurname(res.surname);
+                setEmail(oldEmail);
+                setFetched(true);
+                setLoading(false);
+                //console.log(res);
+            }
+        ).catch(err => {
+            //console.log(err);
+            setResponse("");
+            setErrMessage(err.message);
+            setLoading(false);
+        })
+    }
+
+    function reset(){
+        setFetched(false);
+        setEmail("");
+        setName("");
+        setSurname("");
+    }
     function updateProfile(){
         setLoading(true);
         editProfile({oldemail:oldEmail, email:email, name:name, surname:surname}).then(
@@ -25,6 +54,7 @@ function ProfileUpdatePage(props){
                 setName("");
                 setSurname("");
                 setLoading(false);
+                setFetched(false);
                 //console.log(res);
             }
         ).catch(err => {
@@ -44,12 +74,20 @@ function ProfileUpdatePage(props){
             <AppNavbar/>
 
             <div className="CenteredButton">
-
                 <Form className="form">
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="text-info">Email address</Form.Label>
-                        <Form.Control style={{width: "400px", alignSelf:"center", margin:"auto", marginBottom:"4px"}} value={oldEmail} type="email" placeholder="Email address" onChange={e => setOldEmail(e.target.value)}/>
-                        <hr style={{color:"white", width:"25%", alignSelf:"center", marginLeft:"auto", marginRight:"auto"}}/>
+                        {fetched?
+                            <Form.Label className="text-white">: {oldEmail}</Form.Label>:
+                            <Form.Control style={{width: "400px", alignSelf:"center", margin:"auto", marginBottom:"4px"}} value={oldEmail} type="email" disabled={fetched} placeholder="Email address" onChange={e => setOldEmail(e.target.value)}/>
+                        }
+                        </Form.Group>
+                    <Button type="submit" variant="outline-info" style={{borderWidth:"2px"}} className="HomeButton"  onClick={(e) => {e.preventDefault(); fetched? reset():getProfile(oldEmail);}}>{fetched? "Change profile to edit":"Get profile to update"}</Button>
+                </Form>
+                {fetched && <>
+                    <hr style={{color:"white", width:"25%", alignSelf:"center", marginLeft:"auto", marginRight:"auto"}}/>
+                    <Form className="form">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label style={{marginTop:"8px"}} className="text-info">New Email address</Form.Label>
                         <Form.Control style={{width: "400px", alignSelf:"center", margin:"auto"}} value={email} type="email" placeholder="New email" onChange={e => setEmail(e.target.value)}/>
                         <Form.Label style={{marginTop:"8px"}} className="text-info">Name</Form.Label>
@@ -57,8 +95,8 @@ function ProfileUpdatePage(props){
                         <Form.Label style={{marginTop:"8px"}} className="text-info">Surname</Form.Label>
                         <Form.Control style={{width: "400px", alignSelf:"center", margin:"auto"}} value={surname} type="text" placeholder="Surname" onChange={e => setSurname(e.target.value)}/>
                     </Form.Group>
-                    <Button type="submit" variant="outline-info" style={{borderWidth:"2px"}} className="HomeButton" onClick={(e) => {e.preventDefault(); updateProfile();}}>Update profile</Button>
-                </Form>
+                    <Button type="submit" variant="outline-info" style={{borderWidth:"2px"}} className="HomeButton" onClick={(e) => {e.preventDefault(); if(!e.currentTarget.form.checkValidity()) console.log("here");  updateProfile();}}>Update profile</Button>
+                </Form> </>}
                 <hr style={{color:"white", width:"90%", alignSelf:"center", marginLeft:"auto", marginRight:"auto", marginTop:"20px"}}/>
                 {loading? <Spinner style={{alignSelf:"center", marginLeft:"auto", marginRight:"auto", marginTop:"20px"}} animation="border" variant="info" /> :
                     <>
