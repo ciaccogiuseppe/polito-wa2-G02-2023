046206f205e2a69449417a72dd95f14f44b5fbc3
ciaccogiuseppe <s295982@studenti.polito.it>
2023-05-05 20:07:33 +0200

    feat: adding attachment controller

---
+++ server/src/main/kotlin/it/polito/wa2/server/ticketing/attachment/AttachmentController.kt
@@ -0,0 +1,14 @@
+package it.polito.wa2.server.ticketing.attachment
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class AttachmentController (private val attachmentService : AttachmentService) {
+    @GetMapping("/API/attachment/{attachmentId}")
+    fun getAttachment(@PathVariable attachmentId: String) : AttachmentDTO {
+        return attachmentService.getAttachment(attachmentId)
+    }
+}
