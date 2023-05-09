f528867d37b05de0a297d57ca05cd5971428eea6
ciaccogiuseppe <s295982@studenti.polito.it>
2023-05-09 15:04:45 +0200

    test: adding Profile tests

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation ("org.testcontainers:junit-jupiter:1.16.3")
+	testImplementation("org.testcontainers:postgresql:1.16.3")
 }
 
 tasks.withType<KotlinCompile> {
@@ -35,4 +37,10 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.testcontainers:testcontainers-bom:1.16.3")
+	}
 }
