c4b6d55498c989c95635aa678b07d00ffb948604
Giacomo <s301311@studenti.polito.it>
2023-05-11 11:14:08 +0200

    feat: added API to get filtered ticketHistory

--- server/src/main/kotlin/it/polito/wa2/server/profiles/Profile.kt
+++ server/src/main/kotlin/it/polito/wa2/server/profiles/Profile.kt
@@ -44,5 +44,5 @@ class Profile {
 }
 
 enum class ProfileRole {
-    CUSTOMER, EXPERT, ADMIN
+    CUSTOMER, EXPERT, MANAGER, ADMIN
 }
--- server/src/main/kotlin/it/polito/wa2/server/ticketing/tickethistory/TicketHistoryServiceImpl.kt
+++ server/src/main/kotlin/it/polito/wa2/server/ticketing/tickethistory/TicketHistoryServiceImpl.kt
@@ -1,17 +1,23 @@
 package it.polito.wa2.server.ticketing.tickethistory
 
+import it.polito.wa2.server.profiles.Profile
+import it.polito.wa2.server.profiles.ProfileRepository
+import it.polito.wa2.server.profiles.ProfileService
 import it.polito.wa2.server.ticketing.ticket.Ticket
 import it.polito.wa2.server.ticketing.ticket.TicketRepository
 import it.polito.wa2.server.ticketing.ticket.TicketService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.sql.Timestamp
 
 @Service @Transactional
 class TicketHistoryServiceImpl(
     private val ticketHistoryRepository: TicketHistoryRepository,
     private val ticketService: TicketService,
-    private val ticketRepository: TicketRepository
+    private val ticketRepository: TicketRepository,
+    private val profileService: ProfileService,
+    private val profileRepository: ProfileRepository
 ): TicketHistoryService {
     @Transactional(readOnly = true)
     override fun getAllTicketHistory(): List<TicketHistoryDTO> {
@@ -19,13 +25,41 @@ class TicketHistoryServiceImpl(
     }
 
     @Transactional(readOnly = true)
-    override fun getTicketHistory(ticketId: Long): List<TicketHistoryDTO> {
-        val ticket = getTicket(ticketId)
-        return ticketHistoryRepository.findAllByTicket(ticket).map {it.toDTO()}
+    override fun getTicketHistoryFiltered(
+        ticketId: Long?,
+        userId: Long?,
+        updatedAfter: Timestamp?,
+        updatedBefore: Timestamp?,
+        currentExpertId: Long?
+    ): List<TicketHistoryDTO> {
+        var user: Profile? = null
+        var currentExpert: Profile? = null
+        var ticket: Ticket? = null
+
+        if (ticketId != null)
+            ticket = getTicket(ticketId)
+        if (userId != null)
+            user = getProfile(userId)
+        if (currentExpertId != null)
+            currentExpert = getProfile(currentExpertId)
+        return ticketHistoryRepository
+            .findAll()
+            .filter {
+                (ticket == null || it.ticket == ticket) &&
+                        (user == null || it.user == user) &&
+                        (updatedAfter == null || it.updatedTimestamp!!.after(updatedAfter) || it.updatedTimestamp!!.equals(updatedAfter)) &&
+                        (updatedBefore == null || it.updatedTimestamp!!.before(updatedBefore) || it.updatedTimestamp!!.equals(updatedBefore)) &&
+                        (currentExpert == null || it.currentExpert == currentExpert)
+            }.map { it.toDTO() }
     }
 
     private fun getTicket(ticketId: Long): Ticket {
         val ticketDTO = ticketService.getTicket(ticketId)
         return ticketRepository.findByIdOrNull(ticketDTO.ticketId)!!
+    }
+
+    private fun getProfile(profileId: Long): Profile {
+        val profileDTO = profileService.getProfileById(profileId)
+        return profileRepository.findByEmail(profileDTO.email)!!
     }
 }
