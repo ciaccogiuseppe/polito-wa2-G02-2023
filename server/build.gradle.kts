c93bae4a34a384b3a0b08d36a13b64654b98f813
Giuseppe Ciacco <s295982@studenti.polito.it>
2023-05-12 14:00:37 +0200

    feat: adding dependencies for security

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
