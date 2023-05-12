f4e55bba905599e17d7fce4b4269dfe9dd2f3237
Giacomo <s301311@studenti.polito.it>
2023-05-12 13:22:49 +0200

    fix: updated records.sql

--- server/src/main/resources/records.sql
+++ server/src/main/resources/records.sql
@@ -1,7 +1,19 @@
-/*insert into profiles(email, name, surname) values ('mario.rossi@polito.it', 'Mario', 'Rossi');
-insert into profiles(email, name, surname) values ('john.doe@polito.it', 'John', 'Doe');
-insert into profiles(email, name, surname) values ('jane.doe@polito.it', 'Jane', 'Doe');*/
+BEGIN;
+DO
+$$
+DECLARE
+    id bigint;
+BEGIN
+select nextval('profiles_id_seq') into id;
+insert into profiles(profile_id, email, name, surname, role) values (id, 'mario.rossi@polito.it', 'Mario', 'Rossi', 'EXPERT');
+select nextval('profiles_id_seq') into id;
+insert into profiles(profile_id, email, name, surname, role) values (id, 'john.doe@polito.it', 'John', 'Doe', 'EXPERT');
+select nextval('profiles_id_seq') into id;
+insert into profiles(profile_id, email, name, surname, role) values (id, 'jane.doe@polito.it', 'Jane', 'Doe', 'ADMIN');
 
 insert into products(product_id, name, brand) values ('1234512345123', 'PC', 'HP');
 insert into products(product_id, name, brand) values ('5432154321321', 'iPhone', 'Apple');
 insert into products(product_id, name, brand) values ('1234567890123', 'smartphone', 'Samsung');
+END;
+$$;
+COMMIT;
