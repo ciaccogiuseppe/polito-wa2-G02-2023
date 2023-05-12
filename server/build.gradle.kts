644d0d312727cfda74697e9b0f30ae0f8b719329
Giuseppe Ciacco <s295982@studenti.polito.it>
2023-05-12 14:01:57 +0200

    feat: adding security test libraries

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation ("org.testcontainers:junit-jupiter:1.16.3")
 	testImplementation("org.testcontainers:postgresql:1.16.3")
 }
