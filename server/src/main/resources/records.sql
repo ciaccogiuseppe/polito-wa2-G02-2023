640cdc8a2847e13f1eb52dab408d114cb4b5e5e2
Giacomo <s301311@studenti.polito.it>
2023-05-26 21:37:56 +0200

    fix: updated records.sql

--- server/src/main/resources/records.sql
+++ server/src/main/resources/records.sql
@@ -5,11 +5,15 @@ DECLARE
     id bigint;
 BEGIN
 select nextval('profiles_id_seq') into id;
-insert into profiles(profile_id, email, name, surname, role) values (id, 'mario.rossi@polito.it', 'Mario', 'Rossi', 'EXPERT');
+insert into profiles(id, email, name, surname, role) values (id, 'client1@polito.it', 'ClientA', 'PoliTo', 'CLIENT');
 select nextval('profiles_id_seq') into id;
-insert into profiles(profile_id, email, name, surname, role) values (id, 'john.doe@polito.it', 'John', 'Doe', 'EXPERT');
+insert into profiles(id, email, name, surname, role) values (id, 'client2@polito.it', 'ClientB', 'PoliTo', 'CLIENT');
 select nextval('profiles_id_seq') into id;
-insert into profiles(profile_id, email, name, surname, role) values (id, 'jane.doe@polito.it', 'Jane', 'Doe', 'ADMIN');
+insert into profiles(id, email, name, surname, role) values (id, 'expert1@polito.it', 'ExpertA', 'PoliTo', 'EXPERT');
+select nextval('profiles_id_seq') into id;
+insert into profiles(id, email, name, surname, role) values (id, 'expert2@polito.it', 'ExpertB', 'PoliTo', 'EXPERT');
+select nextval('profiles_id_seq') into id;
+insert into profiles(id, email, name, surname, role) values (id, 'manager@polito.it', 'Manager', 'PoliTo', 'MANAGER');
 
 insert into products(product_id, name, brand) values ('1234512345123', 'PC', 'HP');
 insert into products(product_id, name, brand) values ('5432154321321', 'iPhone', 'Apple');
